772d5d73246b025a5b375a1501d4a45e78529488
Georges Monette <georges@yorku.ca>
2020-08-18 09:18:46 -0400

    large commit

--- R/mytools.R
+++ R/mytools.R
@@ -10,8 +10,11 @@ mygitinstall <- function(x,...) devtools::install_github(x,build_opts = NULL, ..
 #' Simple speed tests
 #' 
 #' @export
-stest <- function() benchmark(
-  exp = {for (i in 1:10^6) exp(i)},
-  mult_diag = {for (i in 1:10^2) diag(1:100) %*% diag(1:100)},
-  svd_singular = {for( i in 1:2) svd(matrix(1:100, 100,100))},
-  svd_I = {for( i in 1:2) svd((diag(100)))})
+stest <- function() {
+  require(rbenchmark)
+  benchmark(
+    exp = {for (i in 1:10^6) exp(i)},
+    mult_diag = {for (i in 1:10^2) diag(1:100) %*% diag(1:100)},
+    svd_singular = {for( i in 1:2) svd(matrix(1:100, 100,100))},
+    svd_I = {for( i in 1:2) svd((diag(100)))})
+}
