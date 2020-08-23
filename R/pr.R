f95bf19ba7d4eb5950ddc14f6e43b0b7d3db4c86
Georges Monette <georges@yorku.ca>
2020-08-23 13:10:22 -0400

    print arrays laically

---
+++ R/pr.R
@@ -0,0 +1,118 @@
+#' Take a slice of an array selecting indices along a margin
+#' 
+#' Performs the same role as selecting with subscripts except that
+#' this is easier to use when dimension of the array is not known.
+#' Its application is limited, however, to selecting single slices
+#' along a single dimension.
+#' 
+#' @param a an array
+#' @param MARGIN a single dimension along which to select a slice (i.e. a section)
+#' @param ind the level at which to select the slice. If ind is out of range,
+#'        slice returns NAs with the correct structure
+#' @examples
+#' Titanic
+#' slice(Titanic, 1, 1)  
+#' slice(slice(Titanic, 1, 1), 1, 2)  
+#' @export
+slice <- function(a, MARGIN, ind) {
+  ret <- array(a[slice.index(a, MARGIN) == ind], dim(a)[-MARGIN])
+  dimnames(ret) <- dimnames(a)[-MARGIN]
+  ret
+}
+slice_alt <- function(a, MARGIN, ind) {
+  # returns an error if ind is out of bounds 
+  structure(a[slice.index(a, MARGIN) == ind], 
+            dim = dim(a)[-MARGIN],
+            dimnames = dimnames(a)[-MARGIN])
+}
+#' Alternative printing for special purposes
+#' 
+#' @param x an object to be printed
+#' @param ... additional arguments
+#' 
+#' @examples
+#' pr(Titanic)
+#' @export
+pr <- function(x, ...) UseMethod('pr')
+#' @describeIn pr default method
+#' @export
+pr.default <- function(x, ...) print(x, ...)
+#' @describeIn pr array method
+#' @export
+pr.array <- function(x, ...) {
+  if(length(dim(x)) < 3 ) print(x,...)
+  else {
+    x <- aperm(x, c(length(dim(x)),seq_len(length(dim(x))-1)))
+    dn <- dimnames(x)[[1]]
+    head <- names(dimnames(x))[1]
+    for(i in seq_len(dim(x)[1])) {
+      cat(paste0(head,": ", dn[i], '\n'))
+      pr(slice(x,1,i),...)
+    }
+  }
+}
+#' @describeIn pr table method
+#' @export
+pr.table <- function(x, ...) pr.array(x, ...)
+#' 
+#' Turn an array into a list of smaller arrays sectioning along a margin
+#' 
+#' @param a array
+#' @param MARGIN vector of dimensions along which to section
+#' 
+#' @returns list of subarrays with names consisting of headings for each subarray
+#'
+#' @examples 
+#' a <- Titanic
+#' library(spida2)
+#' tab(a)
+#' tab(aperm(a,c(2,4,1,3))
+#' tab(aperm(a,c(2,4,1,3)), pct = c(1,3,4))
+#' round(tab(aperm(a,c(2,4,1,3)), pct = c(1,3,4)),1)
+#' list_array(round(tab(aperm(a,c(2,4,1,3)), pct = c(1,3,4)),1),3:4)
+#' 
+#' list_array(tab(a),3:4)
+#' list_array(a,3:4)
+#' list_array(tab(a, pct = c(1,3:4),3:4)
+#' @export
+list_array <- function(a, MARGIN) {
+  inds <- slice.index(a, MARGIN)
+  # disp(inds)
+  dim_out <- dim(a)[-MARGIN]
+  # disp(dim_out)
+  dn_out <- dimnames(a)[-MARGIN]
+  # disp(dn_out)
+  # disp(unique(c(inds)))
+  ret <- lapply(unique(c(inds)), function(i) {
+    sub_arr <- a[inds == i]
+    dim(sub_arr) <- dim_out
+    dimnames(sub_arr) <- dn_out
+    sub_arr
+  })
+  heads <- dimnames(a)[MARGIN]
+  # disp(heads)
+  labs <- do.call(expand.grid, heads)
+  # disp(labs)
+  # pad
+  maxlen <- max(nchar(names(labs)))
+  pad <- lapply(maxlen - nchar(names(labs)), function(iter) paste0(rep('_', iter), collapse = ''))
+  names(labs) <- paste0(pad, names(labs) )
+  labs <- lapply(seq_along(labs), function(i) paste0(names(labs)[i],': ', labs[[i]],'\n'))
+  # disp(labs)
+  labs <- do.call(paste0,labs)
+  labs <- gsub('_',' ', labs)
+  names(ret) <- labs
+  class(ret) <- 'list_array'
+  ret
+}
+#' @export
+print.list_array <- function(x,...) {
+  for(i in seq_along(x)) {
+    cat('\n')
+    cat(names(x)[i])
+    cat('\n')
+    print(x[[i]],...)
+  }
+  invisible(x)
+}
+
